774189363dccd77d7bc1161bf9aa27abf79fc1de
Chris Wise <cwise@murmurinformatics.com>
2012-05-31 08:08:54 -0400

    enhanced statistic class

--- app/models/statistic.rb
+++ app/models/statistic.rb
@@ -12,4 +12,36 @@ class Statistic
       @player_statistics=attributes[:player_statistics] if attributes.has_key?(:player_statistics)
 	  end
 	end
+
+  def touchdowns
+    player_statistics.sum(&:touchdowns)
+  end
+  
+  def games_played
+    player_statistics.sum(&:games_played)
+  end
+  
+  def converts_1
+    player_statistics.sum(&:converts_1)
+  end  
+  
+  def converts_2
+    player_statistics.sum(&:converts_2)
+  end  
+  
+  def field_goals
+    player_statistics.sum(&:field_goals)
+  end  
+  
+  def interceptions
+    player_statistics.sum(&:interceptions)
+  end
+  
+  def sacks
+    player_statistics.sum(&:sacks)
+  end
+  
+  def points
+    player_statistics.sum(&:points)
+  end  
 end
