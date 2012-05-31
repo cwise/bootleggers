91f20037c19ba98b2a10428940fd4ee8b9f3d957
Chris Wise <cwise@murmurinformatics.com>
2012-05-31 06:10:30 -0400

    added support for rails_admin and CanCan

--- Gemfile
+++ Gemfile
@@ -2,7 +2,9 @@ source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
 gem 'devise'
+gem 'cancan'
 gem 'pg'
+gem 'rails_admin'
 
 group :assets do
   gem 'sass-rails',   '~> 3.2.3'
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,6 +1,6 @@
 class Player < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :retired
   validates :first_name, presence: true
-  validates :last_name, presence; true
+  validates :last_name, presence: true
   has_many :player_statistics
 end
